a79cef5ebe8b7c1d86f418fa796460dc155cbfad
haitham <haithamzghidaa@gmail.com>
2022-04-19 15:40:23 +0100

    5eme push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,19 +5,29 @@ use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         $product=$this->getDoctrine()->getRepository(Product::class)->findAll();
 
+        $error = $authenticationUtils->getLastAuthenticationError();
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'product' => $product]);
+            'product' => $product,'last_username' => $lastUsername, 'error' => $error]);
+
+
+            
+    
+            
         
     }
 
